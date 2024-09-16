502dca9e8198dc30e15a4e7dbde975229638a7d2
IvanaMiovcic <ivanamiovcic@gmail.com>
2024-09-16 13:50:54 -0400

    Fix for names to fit project assignment guidelines

--- assignment1/q1_bank_kb.pl
+++ assignment1/q1_bank_kb.pl
@@ -19,62 +19,62 @@ month(7). month(8). month(9). month(10). month(11). month(12).
 %%%%% SECTION: q1_kb
 %%%%% You should put the atomic statements in your KB below in this section
 
-hasAccount(ada, td, 5000).
-hasAccount(alan, cibc, 9500).
-hasAccount(margaret, rbc, 12000).
-hasAccount(grace, scotiabank, 500).
-hasAccount(neil, cibc, 6000).
-hasAccount(grace, bmo, 4000).
-hasAccount(elon, wellsfargo, 100000).
-hasAccount(warren, wellsfargo, 100000000).
-hasAccount(elon, td, 6000).
-hasAccount(elon, nationalbank, 800).
-hasAccount(bill, bmo, 15000).
-hasAccount(bill, scotiabank, 4000).
-hasAccount(bill, rbc, 10).
-hasAccount(justin, td, 300).
-hasAccount(oliver, scotiabank, 90000).
-hasAccount(bill, td, 500).
-hasAccount(bill, nationalbank, 100).
-hasAccount(oliver, nationalbank, 15000).
-hasAccount(oliver, cibc, 3000).
-hasAccount(alan, rbc, 9000).
-hasAccount(margaret, nationalbank, 2000).
-hasAccount(justin, nationalbank, 10000).
+hasAccount(ada_lovelace, td, 5000).
+hasAccount(alan_turing, cibc, 9500).
+hasAccount(margaret_hamilton, rbc, 12000).
+hasAccount(grace_hopper, scotiabank, 500).
+hasAccount(neil_armstrong, cibc, 6000).
+hasAccount(grace_hopper, bmo, 4000).
+hasAccount(elon_musk, wells_fargo, 100000).
+hasAccount(warren_buffet, wells_fargo, 100000000).
+hasAccount(elon_musk, td, 6000).
+hasAccount(elon_musk, national_bank_of_canada, 800).
+hasAccount(bill_gates, bmo, 15000).
+hasAccount(bill_gates, scotiabank, 4000).
+hasAccount(bill_gates, rbc, 10).
+hasAccount(justin_trudeau, td, 300).
+hasAccount(steve_jobs, scotiabank, 90000).
+hasAccount(bill_gates, td, 500).
+hasAccount(bill_gates, national_bank_of_canada, 100).
+hasAccount(steve_jobs, national_bank_of_canada, 15000).
+hasAccount(steve_jobs, cibc, 3000).
+hasAccount(alan_turing, rbc, 9000).
+hasAccount(margaret_hamilton, national_bank_of_canada, 2000).
+hasAccount(justin_trudeau, national_bank_of_canada, 10000).
 
-lives(ada, sanfrancisco).
-lives(alan, london).
-lives(margaret, toronto).
-lives(grace, toronto).
-lives(neil, tokyo).
-lives(elon, losangeles).
-lives(warren, newyork).
-lives(bill, vancouver).
-lives(justin, ottawa).
-lives(oliver, toronto).
+lives(ada_lovelace, sanfrancisco).
+lives(alan_turing, london).
+lives(margaret_hamilton, toronto).
+lives(grace_hopper, toronto).
+lives(neil_armstrong, tokyo).
+lives(elon_musk, losangeles).
+lives(warren_buffet, newyork).
+lives(bill_gates, vancouver).
+lives(justin_trudeau, ottawa).
+lives(steve_jobs, toronto).
 
-created(ada, td, 1, 2024).
-created(alan, cibc, 4, 2018).
-created(margaret, rbc, 7, 1990).
-created(grace, scotiabank, 9, 2023).
-created(neil, cibc, 4, 2018).
-created(grace, bmo, 4, 2021).
-created(elon, wellsfargo, 1, 2000). 
-created(warren, wellsfargo, 9, 1999).
-created(elon, td, 1, 2024).
-created(elon, nationalbank, 5, 2017).
-created(bill, bmo, 3, 2022).
-created(bill, nationalbank, 8, 2017).
-created(justin, td, 12, 2012).
+created(ada_lovelace, td, 1, 2024).
+created(alan_turing, cibc, 4, 2018).
+created(margaret_hamilton, rbc, 7, 1990).
+created(grace_hopper, scotiabank, 9, 2023).
+created(neil_armstrong, cibc, 4, 2018).
+created(grace_hopper, bmo, 4, 2021).
+created(elon_musk, wells_fargo, 1, 2000). 
+created(warren_buffet, wells_fargo, 9, 1999).
+created(elon_musk, td, 1, 2024).
+created(elon_musk, national_bank_of_canada, 5, 2017).
+created(bill_gates, bmo, 3, 2022).
+created(bill_gates, national_bank_of_canada, 8, 2017).
+created(justin_trudeau, td, 12, 2012).
 created(oliver, scotiabank, 9, 2017).
-created(bill, td, 4, 2022).
-created(bill, scotiabank, 8, 2023).
-created(bill, rbc, 10, 2021).
-created(oliver, nationalbank, 10, 2017).
-created(oliver, cibc, 2, 2019).
-created(alan, rbc, 11, 2018).
-created(margaret, nationalbank, 2, 2017).
-created(justin, nationalbank, 12, 2017).
+created(bill_gates, td, 4, 2022).
+created(bill_gates, scotiabank, 8, 2023).
+created(bill_gates, rbc, 10, 2021).
+created(steve_jobs, national_bank_of_canada, 10, 2017).
+created(steve_jobs, cibc, 2, 2019).
+created(alan_turing, rbc, 11, 2018).
+created(margaret_hamilton, national_bank_of_canada, 2, 2017).
+created(justin_trudeau, national_bank_of_canada, 12, 2017).
 
 %%%%% END
 % DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
