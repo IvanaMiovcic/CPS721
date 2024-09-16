d98cf1ae3576995091c64e02a0b3661f70f1b5fb
IvanaMiovcic <ivanamiovcic@gmail.com>
2024-09-16 13:30:15 -0400

    Add files for assignment 1

---
+++ assignment1/q1_bank_kb.pl
@@ -0,0 +1,80 @@
+
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME:Ivana Miovcic 
+%%%%% NAME:Mishelle Bitman
+%%%%% NAME:Daniel Persaud
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the already included comment lines below
+%
+
+month(1). month(2). month(3). month(4). month(5). month(6).
+month(7). month(8). month(9). month(10). month(11). month(12).
+
+%%%%% SECTION: q1_kb
+%%%%% You should put the atomic statements in your KB below in this section
+
+hasAccount(ada, td, 5000).
+hasAccount(alan, cibc, 9500).
+hasAccount(margaret, rbc, 12000).
+hasAccount(grace, scotiabank, 500).
+hasAccount(neil, cibc, 6000).
+hasAccount(grace, bmo, 4000).
+hasAccount(elon, wellsfargo, 100000).
+hasAccount(warren, wellsfargo, 100000000).
+hasAccount(elon, td, 6000).
+hasAccount(elon, nationalbank, 800).
+hasAccount(bill, bmo, 15000).
+hasAccount(bill, scotiabank, 4000).
+hasAccount(bill, rbc, 10).
+hasAccount(justin, td, 300).
+hasAccount(oliver, scotiabank, 90000).
+hasAccount(bill, td, 500).
+hasAccount(bill, nationalbank, 100).
+hasAccount(oliver, nationalbank, 15000).
+hasAccount(oliver, cibc, 3000).
+hasAccount(alan, rbc, 9000).
+hasAccount(margaret, nationalbank, 2000).
+hasAccount(justin, nationalbank, 10000).
+
+lives(ada, sanfrancisco).
+lives(alan, london).
+lives(margaret, toronto).
+lives(grace, toronto).
+lives(neil, tokyo).
+lives(elon, losangeles).
+lives(warren, newyork).
+lives(bill, vancouver).
+lives(justin, ottawa).
+lives(oliver, toronto).
+
+created(ada, td, 1, 2024).
+created(alan, cibc, 4, 2018).
+created(margaret, rbc, 7, 1990).
+created(grace, scotiabank, 9, 2023).
+created(neil, cibc, 4, 2018).
+created(grace, bmo, 4, 2021).
+created(elon, wellsfargo, 1, 2000). 
+created(warren, wellsfargo, 9, 1999).
+created(elon, td, 1, 2024).
+created(elon, nationalbank, 5, 2017).
+created(bill, bmo, 3, 2022).
+created(bill, nationalbank, 8, 2017).
+created(justin, td, 12, 2012).
+created(oliver, scotiabank, 9, 2017).
+created(bill, td, 4, 2022).
+created(bill, scotiabank, 8, 2023).
+created(bill, rbc, 10, 2021).
+created(oliver, nationalbank, 10, 2017).
+created(oliver, cibc, 2, 2019).
+created(alan, rbc, 11, 2018).
+created(margaret, nationalbank, 2, 2017).
+created(justin, nationalbank, 12, 2017).
+
+%%%%% END
+% DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
