66825899da6419a7473a59d3a92637ca18ad96fc
MishelleBitman <70034068+MishelleBitman@users.noreply.github.com>
2024-10-04 12:12:20 -0400

    Update q2_equal_entries.pl

--- assignment2/q2_equal_entries.pl
+++ assignment2/q2_equal_entries.pl
@@ -2,9 +2,9 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
-%%%%% NAME:
+%%%%% NAME: Mishelle Bitman
+%%%%% NAME: Ivana Miovcic
+%%%%% NAME: Daniel Persaud
 %
 % Add the required rules in the corresponding sections. 
 % If you put the rules in the wrong sections, you will lose marks.
@@ -14,3 +14,9 @@
 
 %%%%% SECTION: equalEntries
 %%%%% Put your rules for equalEntries below
+
+equalEntries([H], [H], [true]).
+equalEntries([H], [H2], [false]) :- not(H = H2).
+equalEntries([H | T], [H | T2], [true | T3]) :- equalEntries(T, T2, T3).
+equalEntries([H | T], [H2 | T2], [false | T3]) :- not (H = H2), equalEntries(T, T2, T3).
+
