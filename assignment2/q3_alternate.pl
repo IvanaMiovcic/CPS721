f380f5d768191206a4fb302d58bd33e5a1ce2530
IvanaMiovcic <ivanamiovcic@gmail.com>
2024-10-05 12:11:56 -0400

    Add solution for q3

--- assignment2/q3_alternate.pl
+++ assignment2/q3_alternate.pl
@@ -2,9 +2,9 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
-%%%%% NAME:
+%%%%% NAME:Ivana Miovcic 
+%%%%% NAME:Mishelle Bitman
+%%%%% NAME:Daniel Persaud
 %
 % Add the required rules in the corresponding sections. 
 % If you put the rules in the wrong sections, you will lose marks.
@@ -15,3 +15,8 @@
 %%%%% SECTION: alternatePlusMinus
 %%%%% Put your rules for alternatePlusMinus and any helper predicates below
 
+alternatePlusMinus([], 0).
+alternatePlusMinus([H|T], Sum) :- alternatePlusMinus(T, Sum2, add), Sum is Sum2 + H.
+alternatePlusMinus([], 0, Op).
+alternatePlusMinus([H|T], Sum, Op) :- Op = add, alternatePlusMinus(T, Sum2, sub), Sum is Sum2 + H.
+alternatePlusMinus([H|T], Sum, Op) :- Op = sub, alternatePlusMinus(T, Sum2, add), Sum is Sum2 - H.
